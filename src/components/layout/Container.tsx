d1ba76d1ae5ddba01f70b5dce1cc91f007b4341e
Estebes Mukambetov <estebesm95@gmail.com>
2023-09-17 19:48:05 +0600

    detail page

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,7 +1,7 @@
 import { ChildrenProps } from "types/common";
 
 const Container = ({ children }: ChildrenProps) => {
-  return <div className="mx-auto max-w-md px-4 pt-8 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl text-slate-600">{children}</div>;
+  return <div className="mx-auto max-w-md px-4 pt-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl text-slate-600">{children}</div>;
 };
 
 export default Container;

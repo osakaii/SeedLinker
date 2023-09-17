46a5367eb785065f9d4ae7d26675de152ef02de6
Muhammad <mdzhanov@gmail.com>
2023-09-17 17:52:29 +0600

    fix

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { AuthRoutes } from "modules/auth/routes";
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Root from "./Root";
 import { DashboardRoutes } from "modules/dashboard/routes";
+import { FranchiseRoutes } from "modules/franchise/routes";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -9,6 +10,7 @@ const router = createBrowserRouter(
       {AuthRoutes}
       <Route path="" element={<Root />}>
         {DashboardRoutes}
+        {FranchiseRoutes}
       </Route>
     </Route>
   )
